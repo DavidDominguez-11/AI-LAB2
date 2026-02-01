c93de500ba48a7c9f820fdc54a1d6644b236ee9d
David Dominguez <dominguezdavid396@gmail.com>
2026-01-31 22:02:56 -0600

    finish task 2 full

--- task2.py
+++ task2.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
 
 
 dataset = pd.read_csv("dataset_phishing.csv")
@@ -139,3 +141,53 @@ X_scaled = pd.DataFrame(
 # para verrificar mean ≈ 0, std ≈ 1
 print("\nVerificacion de escalado mean ≈ 0, std ≈ 1: ")
 print(X_scaled.describe())
+
+# 4 80/20
+
+print("\n" + "=" * 30)
+print("Split 80/20")
+
+# hacer el split 80 / 20
+X_train, X_test, y_train, y_test = train_test_split(
+    X_2d,
+    y,
+    test_size=0.20,
+    random_state=42,
+    stratify=y
+)
+
+# comprobaciones
+print("Training size:", X_train.shape[0])
+print("Test size:", X_test.shape[0])
+
+print("\nDistribución en training:")
+print(y_train.value_counts(normalize=True))
+
+print("\nDistribución en test:")
+print(y_test.value_counts(normalize=True))
+
+# escalar los datos solo del training set
+scaler2 = StandardScaler()
+scaler2.fit(X_train)
+
+# Transformar training y test
+X_train_scaled = scaler2.transform(X_train)
+X_test_scaled = scaler2.transform(X_test)
+
+# pasarlo a dataframe pq me daba errores
+print("\nX_train_scaled: ")
+X_train_scaled = pd.DataFrame(
+    X_train_scaled,
+    columns=X_2d.columns
+)
+print("\nVerificacion de escalado mean ≈ 0, std ≈ 1: ")
+print(X_train_scaled.describe())
+
+print("\nX_test_scaled: ")
+X_test_scaled = pd.DataFrame(
+    X_test_scaled,
+    columns=X_2d.columns
+)
+print("\nVerificacion de escalado mean ≈ 0, std ≈ 1: ")
+print(X_test_scaled.describe())
+
